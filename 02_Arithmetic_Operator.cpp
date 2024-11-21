2b3fd738eced8590633f679918998d6cee851256
Dev-2504 <jadavdevendrasinh7@gmail.com>
2024-11-21 11:16:30 +0530

    Arithmetic Operator

---
+++ 02_Arithmetic_Operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+main() {
+    int a,b;
+    cout << "Enter a First Value : ";
+    cin >>a;
+    cout << "Enter a Second Value : ";
+    cin >>b;
+
+    cout << "A + B = " << a + b <<endl;
+    cout << "A - B = " << a - b <<endl;
+    cout << "A * B = " << a * b <<endl;
+    cout << "A / B = " << a / b <<endl;
+    cout << "A % B = " << a % b <<endl;
+}
